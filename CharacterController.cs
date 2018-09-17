26ac85fec10501281ae0228a0f584b4e6601e554
Chris Chalmers <chrischalmers68@gmail.com>
2018-09-18 08:12:29 +1000

    Fixed

--- CharacterController.cs
+++ CharacterController.cs
@@ -2,80 +2,73 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class yBotController : MonoBehaviour
 {
     #region Private
 
-    // Serializable
-    [SerializeField]
-    private float _turnSpeed = 5f;
-    [SerializeField]
-    private float _directionDampTime = 0.1f;
-    [SerializeField]
-    private float _speedDampTime = 0.04f;
-    [SerializeField]
-    private float _movementSpeed = 3f;
-    [SerializeField]
-    private float _gravity = -20f;
+    // Serialized
+    [SerializeField] private float _turnSpeed = 360;
+    [SerializeField] private float _turnDampTime = 0.25f;
+    [SerializeField] private float _movementSpeed = 5f;
+    [SerializeField] private float _gravity = -350f;
 
     // Components
-    private Transform _camTransform;
-    private Animator _animator;
     private CharacterController _characterController;
-
+    private Animator _animator;
+    private Transform _cameraTrans;
+    
     // Fields
-    private Vector3 _inputVector;
-    private float _direction;
-    private float _speed;
+    private float _turnAmount;
+    private float _forwardAmount;
 
     // Hashes
-    private int _directionHash;
+    private int _turnHash;
     private int _speedHash;
 
     #endregion
 
-    // Initialize
     private void Start()
     {
         // Components
-        _camTransform = Camera.main.transform as Transform;
-        _animator = GetComponent<Animator>() as Animator;
         _characterController = GetComponent<CharacterController>() as CharacterController;
+        _animator = GetComponent<Animator>() as Animator;
+        _cameraTrans = Camera.main.transform;
 
         // Hashes
-        _directionHash = Animator.StringToHash("Direction");
+        _turnHash = Animator.StringToHash("Turn");
         _speedHash = Animator.StringToHash("Speed");
     }
 
-    // Main tick
-    private void Update()
+    // Physics tick
+    private void FixedUpdate()
     {
-        _inputVector = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical")).normalized;
-        _speed = _inputVector.sqrMagnitude;
+        var h = Input.GetAxis("Horizontal");
+        var v = Input.GetAxis("Vertical");
+        
+        // Camera relative
+        var camForward = Vector3.Scale(_cameraTrans.forward, new Vector3(1, 0, 1)).normalized;
+        var moveDir = (v * camForward) + (h * _cameraTrans.right);
 
-        if (_inputVector != Vector3.zero)
-            inputAxisToWorldSpace();
-
-        _animator.SetFloat(_directionHash, _inputVector.x, _directionDampTime, Time.deltaTime);
-        _animator.SetFloat(_speedHash, _speed, _speedDampTime, Time.deltaTime);
-
+        // Move player
+        if (moveDir.magnitude > 1f)
+            moveDir.Normalize();
 
         if (_characterController.isGrounded)
-            _inputVector *= _movementSpeed;
-        _inputVector.y += _gravity * Time.deltaTime;
-        _characterController.Move(_inputVector * Time.deltaTime);
-    }
+            moveDir *= _movementSpeed;
+        moveDir.y += _gravity * Time.deltaTime;
+        _characterController.Move(moveDir * Time.deltaTime);
 
-    // Converts the input acis to world space and rotates the this smoothly
-    private void inputAxisToWorldSpace()
-    {
-        var camDirection = _camTransform.forward;
-        camDirection.y = 0f;
-        var referentialShift = Quaternion.FromToRotation(Vector3.forward, Vector3.Normalize(camDirection));
-        var moveDirection = referentialShift * _inputVector;
-        var axisSign = Vector3.Cross(moveDirection, transform.forward);
-        var angleRootToMove = Vector3.Angle(transform.forward, moveDirection) * (axisSign.y >= 0 ? -1f : 1f);
-        _direction = angleRootToMove * _turnSpeed;
-        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0f, transform.eulerAngles.y + angleRootToMove, 0f), _turnSpeed * Time.deltaTime);
+        moveDir = transform.InverseTransformDirection(moveDir);
+        moveDir = Vector3.ProjectOnPlane(moveDir, transform.up);
+        _turnAmount = Mathf.Atan2(moveDir.x, moveDir.z);
+        _forwardAmount = moveDir.z;
+
+        // Rotate Character=
+        transform.Rotate(0, _turnAmount * _turnSpeed * Time.deltaTime, 0);
+        
+        // Update animator
+        _animator.SetFloat(_turnHash, _turnAmount, _turnDampTime, Time.deltaTime);
+        _animator.SetFloat(_speedHash, Mathf.Abs(_forwardAmount));
     }
 }
